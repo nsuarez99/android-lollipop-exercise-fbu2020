fdf494599e77d3a0e6d2b117965d928ee8ec1469
nsuarez99 <ngsuarez99@gmail.com>
2020-07-01 13:00:18 -0400

    Initial commit

---
+++ app/src/main/java/com/codepath/android/lollipopexercise/activities/ContactsActivity.java
@@ -0,0 +1,89 @@
+package com.codepath.android.lollipopexercise.activities;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.codepath.android.lollipopexercise.R;
+import com.codepath.android.lollipopexercise.adapters.ContactsAdapter;
+import com.codepath.android.lollipopexercise.models.Contact;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.List;
+
+
+public class ContactsActivity extends AppCompatActivity {
+    private RecyclerView rvContacts;
+    private ContactsAdapter mAdapter;
+    private List<Contact> contacts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts);
+
+        // Find RecyclerView and bind to adapter
+        rvContacts = (RecyclerView) findViewById(R.id.rvContacts);
+
+        // allows for optimizations
+        rvContacts.setHasFixedSize(true);
+
+        // Define 2 column grid layout
+        final GridLayoutManager layout = new GridLayoutManager(ContactsActivity.this, 2);
+
+        // Unlike ListView, you have to explicitly give a LayoutManager to the RecyclerView to position items on the screen.
+        // There are three LayoutManager provided at the moment: GridLayoutManager, StaggeredGridLayoutManager and LinearLayoutManager.
+        rvContacts.setLayoutManager(layout);
+
+        // get data
+        contacts = Contact.getContacts();
+
+        // Create an adapter
+        mAdapter = new ContactsAdapter(ContactsActivity.this, contacts);
+
+        // Bind adapter to list
+        rvContacts.setAdapter(mAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_contacts, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void addNewContact(MenuItem view){
+        Contact newContact = Contact.getRandomContact(ContactsActivity.this);
+        contacts.add(newContact);
+        mAdapter.notifyDataSetChanged();
+
+//        //onclicklistener for snackbar
+//        View.OnClickListener myOnClickListener = new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(ContactsActivity.this, "it is working", Toast.LENGTH_SHORT);
+//            }
+//        };
+//
+//        //snackbar for undo
+//        Snackbar.make((View) view, R.string.snackbar_text, Snackbar.LENGTH_LONG)
+//                .setAction("Undo", myOnClickListener)
+//                .show();
+    }
+}
